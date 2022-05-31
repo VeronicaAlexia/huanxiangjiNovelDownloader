c37bd669aef0d349557f6d99dde82b79df709570
Alex <luanyin4444@qq.com>
2022-05-31 15:20:29 +0800

    更改缓存文件存储位置~

--- main.py
+++ main.py
@@ -15,15 +15,11 @@ class Book:
     def __init__(self, book_id):
         self.book_url = 'http://www.huanxiangji.com/book/{}/'.format(book_id)
         self.book_info_html = HttpUtil.get(self.book_url)
-        self.book_name = re.findall('<h1>(.*?)</h1>', self.book_info_html)[0]
+        self.book_name = re.sub(r"[/\\:*?\"<>|]", "", re.findall('<h1>(.*?)</h1>', self.book_info_html)[0])
         self.book_author = re.findall('<p>作者：(.*?)</p>', self.book_info_html)[0]
         self.book_state = re.findall('>状态：(.*?)</p>', self.book_info_html)[0]
         self.book_update = re.findall('<p>最后更新：(.*?)</p>', self.book_info_html)[0]
         self.introduce = ""
-        if not os.path.exists("./config"):
-            os.mkdir("./config")
-        if not os.path.exists("./novel"):
-            os.mkdir("./novel")
 
     def show_book_information(self):
         self.introduce += "书名：{}\n".format(self.book_name)
@@ -32,6 +28,11 @@ class Book:
         self.introduce += "最后更新：{}\n".format(self.book_update)
         self.introduce += "书籍地址：{}\n".format(self.book_url)
         print(self.introduce, "\n")
+        if not os.path.exists(os.path.join(os.getcwd(), "config", self.book_name)):
+            os.makedirs(os.path.join(os.getcwd(), "config", self.book_name))
+        if not os.path.exists("./novel"):
+            os.mkdir("./novel")
+        write_file("./novel/" + self.book_name + ".txt", 'w', self.introduce)
 
     def get_catalogue(self):
         download_url_list = list()
@@ -44,15 +45,18 @@ class Book:
         return False
 
     def save_chapter(self, catalogue_info):
-        write_file("./novel/" + self.book_name + ".txt", 'w', self.introduce)
+        merge_text = []
         for catalogue in catalogue_info:
             chapter_file_name = catalogue[0].replace(".html", '.txt')
-            if os.path.exists("./config/" + chapter_file_name):
+            if os.path.exists(os.path.join("config", self.book_name, chapter_file_name)):
                 write_file(
                     "./novel/" + self.book_name + ".txt", 'a',
-                    "\n\n\n" + write_file("./config/" + chapter_file_name, 'r')
+                    "\n\n\n" + write_file(os.path.join("config", self.book_name, chapter_file_name), 'r')
                 )
-        self.introduce = ""
+                merge_text.append(chapter_file_name)
+        if merge_text:
+            print(self.book_name, "本地缓存文件一共 {} 章, 已合并完毕".format(len(merge_text)))
+
 
     def get_context(self):
         chapter_info = self.get_catalogue()
@@ -61,14 +65,14 @@ class Book:
             return False
         for catalogue in chapter_info:
             chapter_id, chapter_name = catalogue[0], catalogue[1]
-            if os.path.exists("./config/" + chapter_id.replace(".html", '.txt')):
+            if os.path.exists(os.path.join("config", self.book_name, chapter_id.replace(".html", '.txt'))):
                 continue
             print("开始下载 : {}".format(chapter_name))
             content_html = HttpUtil.get(self.book_url + chapter_id)
             content_info = content_html.split('<div class="content" id="content">')[1].split('</div>')[0]
             content = re.sub('<br />', '\n', content_info).replace('&nbsp;', '').replace(" ", "").split('\n')
             content_text = chapter_name + ''.join(["\n　　" + line for line in content if line.strip() != ''])
-            write_file("./config/" + chapter_id.replace(".html", '.txt'), 'w', content_text)
+            write_file(os.path.join("config", self.book_name, chapter_id.replace(".html", '.txt')), 'w', content_text)
         return chapter_info
 
 
