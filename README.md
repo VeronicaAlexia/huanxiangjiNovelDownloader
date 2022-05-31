2504974297bc94f5131cf1309ae6944191033ca2
Alex <luanyin4444@qq.com>
2022-05-31 17:48:32 +0800

    添加存储路径自定义，添加简繁转换~

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # huanxiangjiNovelDownloader
-huanxiangjiNovelDownloader
+## 小说下载器
+    使用方法: python main.py 命令参数
+    参数说明:
+    -i / --id <input bookid> 
+        <input bookid> 为小说id, 可以是小说的网址也可以是小说的id
+        "http://www.huanxiangji.com/book/19/" 为小说网址, "19" 为小说id
+    -o / --output <output path> 
+        <output path>  为小说下载路径, 默认为当前目录
+    -t / --translate 
+        <translate> 为简繁转换， 默认为简体
+
+
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
 import HttpUtil
+from zhconv import convert
 
 
 def write_file(path: str, mode: str = "a", content: str = ""):
@@ -11,13 +12,15 @@ def write_file(path: str, mode: str = "a", content: str = ""):
 
 
 class Book:
-    def __init__(self, book_id):
-        self.book_url = 'http://www.huanxiangji.com/book/{}/'.format(book_id)
+    def __init__(self, args):
+        self.args = args
+        self.book_url = 'http://www.huanxiangji.com/book/{}/'.format(self.args.id)
         self.book_info_html = HttpUtil.get(self.book_url)
         self.book_name = re.sub(r"[/\\:*?\"<>|]", "", re.findall('<h1>(.*?)</h1>', self.book_info_html)[0])
         self.book_author = re.findall('<p>作者：(.*?)</p>', self.book_info_html)[0]
         self.book_state = re.findall('>状态：(.*?)</p>', self.book_info_html)[0]
         self.book_update = re.findall('<p>最后更新：(.*?)</p>', self.book_info_html)[0]
+        self.out_put_path = os.path.join(os.getcwd(), self.args.output, self.book_name + ".txt")
         self.introduce = ""
 
     def show_book_information(self):
@@ -29,9 +32,9 @@ class Book:
         print(self.introduce, "\n")
         if not os.path.exists(os.path.join(os.getcwd(), "config", self.book_name)):
             os.makedirs(os.path.join(os.getcwd(), "config", self.book_name))
-        if not os.path.exists("./novel"):
-            os.mkdir("./novel")
-        write_file("./novel/" + self.book_name + ".txt", 'w', self.introduce)
+        if not os.path.exists(self.args.output):
+            os.mkdir(self.args.output)
+        write_file(self.out_put_path, 'w', self.introduce)
 
     def get_catalogue(self):
         download_url_list = list()
@@ -42,19 +45,6 @@ class Book:
                     download_url_list.append(i)
             return download_url_list
         return False
-
-    def save_chapter(self, catalogue_info):
-        merge_text = []
-        for catalogue in catalogue_info:
-            chapter_file_name = catalogue[0].replace(".html", '.txt')
-            if os.path.exists(os.path.join("config", self.book_name, chapter_file_name)):
-                write_file(
-                    path=os.path.join("novel", self.book_name + ".txt"),
-                    content="\n\n\n" + write_file(os.path.join("config", self.book_name, chapter_file_name), 'r')
-                )
-                merge_text.append(chapter_file_name)
-        if merge_text:
-            print(self.book_name, "本地缓存文件一共 {} 章, 已合并完毕".format(len(merge_text)))
 
     def get_context(self):
         chapter_info = self.get_catalogue()
@@ -73,19 +63,45 @@ class Book:
             write_file(os.path.join("config", self.book_name, chapter_id.replace(".html", '.txt')), 'w', content_text)
         return chapter_info
 
+    def save_chapter(self, catalogue_info):
+        merge_text = []
+        for catalogue in catalogue_info:
+            chapter_file_name = catalogue[0].replace(".html", '.txt')
+            if os.path.exists(os.path.join("config", self.book_name, chapter_file_name)):
+                read_content = write_file(os.path.join("config", self.book_name, chapter_file_name), 'r')
+                if self.args.traditional:
+                    save_content = "\n\n\n" + convert(read_content, 'zh-hant')
+                else:
+                    save_content = "\n\n\n" + read_content
+                write_file(path=self.out_put_path, content=save_content)
+                merge_text.append(chapter_file_name)
+        if merge_text:
+            print(self.book_name, "本地缓存文件一共 {} 章, 已合并完毕".format(len(merge_text)))
+
 
 def shell():
     import argparse
-    parser = argparse.ArgumentParser(description='小说下载器', usage='python main.py -i <book_id>')
+    parser = argparse.ArgumentParser(
+        description='小说下载器',
+        usage='python main.py'
+              '\n-i <book_id> [required]'
+              '\n-t <true/false>'
+              '\n-o <output_path>',
+        epilog='欢迎使用小说下载器，如果有问题请在Github提交Issues'
+    )
     parser.add_argument('-i', '--id', help='输入书籍id', required=True)
-    book_id = parser.parse_args().id
-    book_config = Book(book_id)
-    book_config.show_book_information()
-    chapter_list = book_config.get_context()
-    if chapter_list:
-        book_config.save_chapter(book_config.get_catalogue())
+    parser.add_argument('-t', '--traditional', default=False, help='繁体转换', action='store_true')
+    parser.add_argument('-o', '--output', default="novel", help='保存到本地文件名')
+    if not parser.parse_args().id.isdigit():
+        print("书籍id必须是数字")
     else:
-        print("没有获取到任何章节信息，下载失败！")
+        book_config = Book(args=parser.parse_args())
+        book_config.show_book_information()
+        chapter_list = book_config.get_context()
+        if chapter_list:
+            book_config.save_chapter(book_config.get_catalogue())
+        else:
+            print("没有获取到任何章节信息，下载失败！")
 
 
 if __name__ == '__main__':
